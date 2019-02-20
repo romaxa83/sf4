9cee94eb097f7459935c351975b00b82235c00a0
roman <romaxa83@ukr.net>
2019-02-20 22:07:23 +0200

    doctrine

---
+++ app/src/Controller/MicroPostController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use App\Form\MicroPostType;
+use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+
+/**
+ * @Route("/micro-post")
+ */
+class MicroPostController
+{
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+    /**
+     * @var MicroPostRepository
+     */
+    private $microPostRepository;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(
+        \Twig_Environment $twig,
+        MicroPostRepository $microPostRepository,
+        FormFactoryInterface $formFactory,
+        EntityManagerInterface $entityManager,
+        RouterInterface $router
+    )
+    {
+        $this->twig = $twig;
+        $this->microPostRepository = $microPostRepository;
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
+    }
+
+    /**
+     * @Route("/",name="micro_post_index")
+     */
+    public function index()
+    {
+        $html = $this->twig->render('micro-post/index.html.twig',[
+            'posts' => $this->microPostRepository->findAll()
+        ]);
+
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/add",name="micro_post_add")
+     */
+    public function add(Request $request)
+    {
+        $microPost = new MicroPost();
+        $microPost->setTime(new \DateTime());
+
+        $form = $this->formFactory->create(
+            MicroPostType::class,$microPost
+        );
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->entityManager->persist($microPost);
+            $this->entityManager->flush();
+
+            return new RedirectResponse(
+                $this->router->generate('micro_post_index')
+            );
+        }
+
+        return new Response(
+            $this->twig->render('micro-post/add.html.twig',[
+                'form' => $form->createView()
+            ])
+        );
+    }
+}
---
+++ app/src/Entity/MicroPost.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string",length=300)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime",length=300)
+     */
+    private $time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time): void
+    {
+        $this->time = $time;
+    }
+}
